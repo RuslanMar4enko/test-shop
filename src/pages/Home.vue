f1c4d57b750c8dc927a7bf4c8a0355559c760f80
RuslanMar4enko <ruslanmarchenko2912@gmail.com>
2019-09-09 10:32:36 +0300

    fix style

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,7 +7,7 @@
           <h3>Выводим комментарии</h3>
           <div class="comments-view">
             <div class="row">
-              <div class="col-4 col-xs-12"
+              <div class="col-md-4 col-xs"
                    v-for="(comment, index) in comments"
                    v-bind:key="index">
                 <router-link :to="{name: 'showComment', query: { data: comment }}">
